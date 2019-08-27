7b59047be8c79c2e7199d7452a494ab646222f3a
Vivi <vvw224@gmail.com>
2019-08-27 10:08:03 +0800

    version 1.0

---
+++ chat.py
@@ -0,0 +1,42 @@
+"""
+chat = []
+with open('input.txt', 'r') as f:
+	for line in f:
+		name, chat = line.split().split(',')
+		chat.append([name, chat])
+"""
+
+def read_file(filename):
+	lines = []
+	with open(filename, 'r', encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+def convert(lines):
+	new = []
+	person = None
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		if person:
+			new.append(person + ':' + line)
+	return new
+
+def write_file(filename, lines):
+	with open(filename, 'w') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+
+def main():
+	lines = read_file('input.txt')
+	lines = convert(lines)
+	write_file('output2.txt', lines)
+
+
+main()
